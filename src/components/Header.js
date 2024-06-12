4017099862c36f6df651fc90781cc6c657cf3724
pt1sp <fcz53eiswy@gmial.com>
2024-06-12 14:38:56 +0900

    go

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-        <div className="logo">
-            <h3>マイプロフィール</h3>
-        </div>
+      <div className="logo">
+        <h3>マイプロフィール</h3>
+      </div>
 
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">ホーム</Link>
-                </li>
-                <li>
-                    <Link to="/PortfolioPage">作品一覧</Link>
-                </li>
-                <li>
-                    <Link to="/BlogPage">ブログ</Link>
-                </li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">ホーム</Link>
+          </li>
+          <li>
+            <Link to="/PortfolioPage">作品一覧</Link>
+          </li>
+          <li>
+            <a href="https://github.com/pt1sp">Github</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import mysqlImage from "../Images/mysql.jpg";
 import jsImage from "../Images/js.png";
 import phpImage from "../Images/php.jpg";
@@ -17,8 +17,7 @@ const HomePage = () => {
       <img src={profileImage} className="profileImage" />
 
       <p>
-        横山龍之介です。普段はwebエンジニア勉強かゲームをしています。遊んでいるゲームでこうなったらいいなと思うことがあったので、
-        PHPとJavaScriptとMySQLを使ってそれが実現できるサイトを作りました。
+        趣味はゲームです。１１歳の頃にはじめたスプラトゥーンというゲームを現在までずっと続けています。中学校ではバスケ部に入って運動してましたが、引退してから体育以外で運動することがなくなり、運動不足に危機感を感じたので、最近懸垂バーを購入して部屋で筋トレをしています。
       </p>
 
       <section className="page-section" id="services">
@@ -37,7 +36,7 @@ const HomePage = () => {
               </span>
               <h4 className="my-3">ランキングサイト</h4>
               <p className="text-muted">
-                普段遊んでいたゲームでのランキングの並び替えや絞り込みが思うようにできなかったり読み込みが遅かったので、自分の思うようにできるサイトを作りました。初めてのサイトかつYouTube等で作り方があるものでもありません。
+                普段遊んでいたゲームでのランキングの並び替えや絞り込みが思うようにできなかったり読み込みが遅かったので、自分の思うようにできるサイトを作りました。プログラムを学んでから初めて作った作品です。
               </p>
             </div>
             <div className="col-md-4">
@@ -47,7 +46,7 @@ const HomePage = () => {
               </span>
               <h4 className="my-3">マッチングシステム</h4>
               <p className="text-muted">
-                これも普段遊んでいるゲームでのこんなものがあったらいいなと思ったため作成したものです。サッカーで言うなら、練習試合にあたることをゲームでする相手を、日付やルールなどを設定して募集することができるものです。
+                これも普段遊んでいるゲームでのこんなものがあったらいいなと思ったため作成したものです。サッカーで例えると練習試合のようなものを自分は普段ゲームでするんですが、その相手チームを日付やルールなどを設定して募集することができるものです。しかし、まだ未完成です。
               </p>
             </div>
             <div className="col-md-4">
@@ -87,73 +86,124 @@ const HomePage = () => {
         </div>
       </section>
       <section class="page-section" id="about">
-            <div class="container">
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase">経歴</h2>
-                    <h3 class="section-subheading text-muted">私の人生の要点をまとめました</h3>
+        <div class="container">
+          <div class="text-center">
+            <h2 class="section-heading text-uppercase">経歴</h2>
+            <h3 class="section-subheading text-muted">
+              私の人生の要点をまとめました
+            </h3>
+          </div>
+          <ul class="timeline">
+            <li>
+              <div class="timeline-image">
+                <img
+                  class="rounded-circle img-fluid"
+                  src={sportsImage}
+                  alt="..."
+                />
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4>2014-2019</h4>
+                  <h4 class="subheading">小中学校</h4>
                 </div>
-                <ul class="timeline">
-                    <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src={sportsImage} alt="..." /></div>
-                        <div class="timeline-panel">
-                            <div class="timeline-heading">
-                                <h4>2014-2019</h4>
-                                <h4 class="subheading">小中学校</h4>
-                            </div>
-                            <div class="timeline-body"><p class="text-muted">小学校の部活で長距離走とドッヂボール、習い事でサッカーと水泳をやっていました。中学校では部活でバスケをやっていました。</p></div>
-                        </div>
-                    </li>
-                    <li class="timeline-inverted">
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src={bokiImage} alt="..." /></div>
-                        <div class="timeline-panel">
-                            <div class="timeline-heading">
-                                <h4>2020-2022</h4>
-                                <h4 class="subheading">高校</h4>
-                            </div>
-                            <div class="timeline-body"><p class="text-muted">総合科の高校に進学し商業化で簿記を学びました。他には商品開発やマーケティングやエクセルのことも学びました。</p></div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src={programImage} alt="..." /></div>
-                        <div class="timeline-panel">
-                            <div class="timeline-heading">
-                                <h4>2023.4</h4>
-                                <h4 class="subheading">専門学校へ進学</h4>
-                            </div>
-                            <div class="timeline-body"><p class="text-muted">進路を簿記とプログラミングで迷った結果プログラミングの方が楽しそうだったのでIT科のある専門学校に進学しました。</p></div>
-                        </div>
-                    </li>
-                    <li class="timeline-inverted">
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src={winImage} alt="..." /></div>
-                        <div class="timeline-panel">
-                            <div class="timeline-heading">
-                                <h4>2023.8</h4>
-                                <h4 class="subheading">ゲーム大会で優勝</h4>
-                            </div>
-                            <div class="timeline-body"><p class="text-muted">趣味でやっているゲームの大会で優勝しました。年に１回しかない大会なのでとてもうれしかったです。</p></div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src={engineerImage} alt="..." /></div>
-                        <div class="timeline-panel">
-                            <div class="timeline-heading">
-                                <h4>2024.2</h4>
-                                <h4 class="subheading">エンジニア</h4>
-                            </div>
-                            <div class="timeline-body"><p class="text-muted">2024年の2月に趣味で繋がったエンジニアの方と就職について話したことをきっかけにこれから何をすればいいのかが明確に想像できました。そのおかげでエンジニアについて勉強することが楽しくなっています。</p></div>
-                        </div>
-                    </li>
-                    <li class="timeline-inverted">
-                        <div class="timeline-image">
-                            <h4>現在
-                            </h4>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </section>
+                <div class="timeline-body">
+                  <p class="text-muted">
+                    小学校の部活で長距離走とドッヂボール、習い事でサッカーと水泳をやっていました。中学校では部活でバスケをやっていました。ほとんど友達が入るからという理由で入りました。
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image">
+                <img
+                  class="rounded-circle img-fluid"
+                  src={bokiImage}
+                  alt="..."
+                />
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4>2020-2022</h4>
+                  <h4 class="subheading">高校</h4>
+                </div>
+                <div class="timeline-body">
+                  <p class="text-muted">
+                    総合科の高校に進学し商業化で簿記を学びました。他には商品開発やマーケティングやエクセルのことも学びました。とても有益な学習だと感じてます。
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-image">
+                <img
+                  class="rounded-circle img-fluid"
+                  src={programImage}
+                  alt="..."
+                />
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4>2023.4</h4>
+                  <h4 class="subheading">専門学校へ進学</h4>
+                </div>
+                <div class="timeline-body">
+                  <p class="text-muted">
+                    過去現在を分析して将来自分の自分に一番適しているものがプログラミングだと思ったのでIT科のある専門学校に進学しました。
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image">
+                <img
+                  class="rounded-circle img-fluid"
+                  src={winImage}
+                  alt="..."
+                />
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4>2023.8</h4>
+                  <h4 class="subheading">ゲーム大会で優勝</h4>
+                </div>
+                <div class="timeline-body">
+                  <p class="text-muted">
+                    趣味でやっているゲームの大会で優勝しました。年に１回しかない大会なのでとてもうれしかったです。
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-image">
+                <img
+                  class="rounded-circle img-fluid"
+                  src={engineerImage}
+                  alt="..."
+                />
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4>2024.2</h4>
+                  <h4 class="subheading">エンジニア</h4>
+                </div>
+                <div class="timeline-body">
+                  <p class="text-muted">
+                    エンジニアになるために勉強中です。周りの友人たちと喋っていると現在時点でも圧倒的な知識の格差があると感じていますが、エンジニアになるためには今の何倍も知識がいると思うので、焦らず着実に勉強していきます。
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image">
+                <h4>現在</h4>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
